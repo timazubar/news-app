00601c0a4d9e699c5e7455699694c6ceccd6b32a
timazubar <timazubar33@gmail.com>
2020-07-15 19:03:22 +0300

    feat: setup request to News API

--- news-app/pages/news.js
+++ news-app/pages/news.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import MainLayout from '../components/MainLayout';
 
-const News = () => {
+const News = ({ news }) => {
   return (
     <MainLayout>
       <h1>News Page</h1>
@@ -11,8 +11,22 @@ const News = () => {
           <a>Back to home</a>
         </Link>
       </p>
+      <pre>{JSON.stringify(news, null, 2)}</pre>
     </MainLayout>
   );
+};
+
+News.getInitialProps = async () => {
+  const response = await fetch(
+    'http://newsapi.org/v2/top-headlines?' +
+      'country=us&' +
+      'apiKey=545054ab22e9474b82aed84d211dcf1b'
+  );
+  const news = await response.json();
+
+  return {
+    news,
+  };
 };
 
 export default News;
